3a8454636c4f4964aaa93ff999a62b9a4628251e
Juan José Peña B <jujpenabe@unal.edu.co>
2023-12-12 14:21:05 -0500

    Added Menu Focus
    Now the focus can be navigated with the keyboard

--- scenes/menu/options/options.gd
+++ scenes/menu/options/options.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Grab the volume slider focus
+	$MarginContainer/VBoxContainer/Volume.grab_focus();
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
